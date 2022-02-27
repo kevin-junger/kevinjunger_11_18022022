2f4ac29d0c7158e688381943193f19337aff7b72
Kevin Junger <kevin.junger@outlook.fr>
2022-02-27 13:21:39 +0100

    organisation et responsive

--- kasa/src/containers/home.jsx
+++ kasa/src/containers/home.jsx
@@ -1,11 +1,10 @@
+import Hero from "../components/hero"
 import Rentals from "../components/rentals"
 
 export default function Home() {
   return(
     <main className="home">
-      <section className="home__hero">
-        <h1>Chez vous, partout et ailleurs</h1>
-      </section>
+      <Hero />
       <Rentals />
     </main>
   )
