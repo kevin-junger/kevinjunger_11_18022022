7babd2920186bd317b6fbb594abecb482db6d1d1
Kevin Junger <kevin.junger@outlook.fr>
2022-03-10 00:32:44 +0100

    underline dynamique sur les liens du menu

--- kasa/src/components/header.jsx
+++ kasa/src/components/header.jsx
@@ -1,14 +1,17 @@
+import { useLocation } from "react-router-dom"
 import { Link } from "react-router-dom"
 import KasaOrange from "../assets/kasaOrange"
 
 export default function Header() {
+  const location = useLocation()
+
   return (
     <header className="header">
       <Link to="/"><KasaOrange /></Link>
       <nav className="header__nav">
         <ul className="header__links">
-          <li><Link to="/">Accueil</Link></li>
-          <li><Link to="/about">A propos</Link></li>
+          <li className={location.pathname === "/" ? "header__links--underline" : ""}><Link to="/">Accueil</Link></li>
+          <li className={location.pathname === "/about" ? "header__links--underline" : ""}><Link to="/about">A propos</Link></li>
         </ul>
       </nav>
     </header>
