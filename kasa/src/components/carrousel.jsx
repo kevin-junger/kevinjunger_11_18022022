53b6b44551613383745158bcd9a15ad2adf98dae
Kevin Junger <kevin.junger@outlook.fr>
2022-03-16 17:10:29 +0100

    fix: dysfonctionnement fleche gauche carrousel

--- kasa/src/components/carrousel.jsx
+++ kasa/src/components/carrousel.jsx
@@ -11,8 +11,8 @@ export default function Carrousel(props) {
      * When the left btn is clicked and carrouselIndex is 0 (which is the first picture), it'll be updated to match the index for the last picture (gallery.length - 1).
      * When the right btn is clicked and carrouselIndex equals gallery.length - 1 (which corresponds to the last picture), it'll be updated with 0.
      */
-
-    (e.nativeEvent.target.parentElement.id === "left" ? 
+    
+    (e.nativeEvent.target.id === "left" ? 
       updateCarrouselIndex((carrouselIndex !== 0 ? carrouselIndex -= 1 : carrouselIndex = gallery.length - 1)) :
       updateCarrouselIndex((carrouselIndex !== gallery.length - 1 ? carrouselIndex += 1 : carrouselIndex = 0))
     )
@@ -20,11 +20,11 @@ export default function Carrousel(props) {
 
   return(
     <div className="carrousel">
-      <div className="carrousel__btn" id="left" onClick={handleClick}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50px" height="50px"><path d="M0 0h24v24H0V0z" fill="none"/><path fill="white" d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/></svg>
+      <div className="carrousel__btn left" onClick={handleClick}>
+        <svg id="left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50px" height="50px"><path d="M0 0h24v24H0V0z" fill="none"/><path fill="white" d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/></svg>
       </div>
-      <div className="carrousel__btn" id="right" onClick={handleClick}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50px" height="50px"><path d="M0 0h24v24H0V0z" fill="none"/><path fill="white" d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/></svg>
+      <div className="carrousel__btn right" onClick={handleClick}>
+        <svg id="right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50px" height="50px"><path d="M0 0h24v24H0V0z" fill="none"/><path fill="white" d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/></svg>
       </div>
       <img className="carrousel__image" src={gallery[carrouselIndex]} alt="" /> {/* uses the carrouselIndex value to choose the corresponding picture in the Array */}
       <div className="carrousel__position">{carrouselIndex + 1 }/{gallery.length}</div> {/* likewise, but +1 - otherwise, it would display "0/5" when the first picture is displayed */}
